64a4350d73393cd1826c743d2bcf6a5626d6ba13
morita taro <morita0079@gmail.com>
2021-07-07 02:35:25 +0900

    add omanko

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@ public class Main {
 	public static void main(String[] args){
 		Hero h1 = new Hero();
 		h1.setName("おちんちん");
+		Hero h2 = new Hero();
+		h2.setName("omanko");
 		System.out.println(h1.name);
+		System.out.println(h2.name);
 
 	}
 }
